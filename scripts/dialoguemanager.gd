eae513780673f68d872775b6d6dfe9bcb9a24dea
2BEHDAMNED <massive2bdamnedfan@gmail.com>
2026-03-29 15:06:26 +0100

    dialogue system: press enter to continue

--- scripts/dialoguemanager.gd
+++ scripts/dialoguemanager.gd
@@ -3,7 +3,11 @@ extends Node
 var container : Control = null
 var portrait  : TextureRect = null
 var dialogue  : Label = null
+var enterquery: Label = null
 var is_active : bool = false
+var is_active_array = false
+
+signal continue_going
 
 func play_list(texts : Array):
 	var index = 0
@@ -16,11 +20,13 @@ func play_dialogue(text : String, is_array: bool = false):
 	if is_active:
 		return
 	
+	if is_array != is_active_array:
+		is_active_array = is_array
+	
 	if not dialogue:
 		_get_dialogue()
 	
 	if dialogue:
-		await get_tree().create_timer(1).timeout
 		container.visible = true
 		is_active = true
 		
@@ -31,15 +37,35 @@ func play_dialogue(text : String, is_array: bool = false):
 			dialogue.visible_characters += 1
 			await get_tree().create_timer(0.05).timeout
 		
-		is_active = false
-		await get_tree().create_timer(2).timeout
 		
-		# maybe wait on input instead of continuing as that makes it harder to read
-		if not is_array:
-			await get_tree().create_timer(2).timeout
-			container.visible = false
-		else:
-			dialogue.visible_characters = 0
+		await get_tree().create_timer(1).timeout
+		var in_the_middle_of_something = false
+		
+		continue_going.connect(func(is_array: bool):
+			if not is_active:
+				return
+			
+			if in_the_middle_of_something:
+				return
+			
+			in_the_middle_of_something = true
+			if not is_array:
+				container.visible = false
+			else:
+				dialogue.visible_characters = 0
+			is_active = false
+		)
+		
+		while is_active:
+			if not enterquery.visible:
+				enterquery.visible = true
+			await get_tree().create_timer(0.001).timeout
+		
+		enterquery.visible = false
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("ui_accept") and is_active:
+		continue_going.emit(is_active_array)
 
 func _get_portrait():
 	if not container:
@@ -49,9 +75,15 @@ func _get_portrait():
 func _get_container():
 	container = get_node("/root/Node3D/ThePlayer/Control/DialogContainer")
 
+func _get_enterquery():
+	enterquery = get_node("/root/Node3D/ThePlayer/Control/DialogContainer/DialogueContainer/PressEnter")
+
 func _get_dialogue():
 	if not container:
 		_get_container()
+	if not enterquery:
+		_get_enterquery()
+	
 	dialogue = get_node("/root/Node3D/ThePlayer/Control/DialogContainer/DialogueContainer/Dialogue")
 	if dialogue:
 		dialogue.text = ""
